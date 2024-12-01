b88557444022eeffb54a9436bf2ba70ea6c766cd
SBence <43538202+SBence@users.noreply.github.com>
2024-12-01 01:07:04 +0100

    Update to Vite 6, add workaround Tabler Icons bug
    See: https://github.com/tabler/tabler-icons/issues/1233

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  resolve: {
+    alias: {
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
 });
